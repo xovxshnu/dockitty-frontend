1828950204459f6afc1c950c0a94543c5665c708
vishnu <cvishnuoff@gmail.com>
2025-06-06 19:24:49 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+  <GoogleOAuthProvider clientId="315445638135-m43ec5hv1qhm32o2kgkeng7qbtvjg2et.apps.googleusercontent.com">
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
   </GoogleOAuthProvider>
 );
